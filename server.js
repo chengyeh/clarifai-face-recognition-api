71a0662fdbc8fef5b79830cf392ba2587b821592
Cheng Yeh Lee <chengyeh@ku.edu>
2018-03-29 20:59:06 -0500

    Seperate the routhing functionality into individual files

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const bcrypt = require('bcrypt-nodejs')
-const knex = require('knex')
+const knex = require('knex');
+const signup = require('./controllers/signup');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
@@ -22,84 +26,8 @@ app.get('/', (req, res) => {
 	res.json(database.users);
 });
 
-app.post('/signin', (req, res) => {
-	const { email, password } = req.body;
-	db.select('*')
-		.from('login')
-		.where('email', email)
-		.then(logoinInfo => {
-			// Compare hash from the login table to the password user submit.
-			bcrypt.compare(password, logoinInfo[0].hash, function(err, isValid) {
-			    if(isValid) {
-			    	db.select('*')
-			    		.from('users')
-			    		.where('email', email)
-			    		.then(user => {
-			    			res.json(user[0]);
-			    		})
-			    		.catch(err => res.status(400).json('error getting user'));
-			    } else {
-			    	res.status(400).json(`email and password don't match`);
-			    }
-			});
-		})
-		.catch(err => res.status(400).json('error getting user'))
-});
-
-app.post('/signup', (req, res) => {
-	const { name, email, password } = req.body;
-
-	bcrypt.hash(password, null, null, function(err, hash) {
-    	// Store hash in the user table.
-    	db.transaction(trx => {
-			trx('login')
-				.insert({email, hash})
-				.returning('email')
-				.then(loginEmail => {
-					return trx('users')
-							.insert({
-								name,
-								email: loginEmail[0],
-								addedon: new Date()
-							})
-							.returning('*')
-							.then(user => res.json(user[0]))
-				})
-				.then(trx.commit)
-				.catch(trx.rollback)
-		})
-		.catch(err => res.status(400).json('unable to sign up'));
-	});
-})
-
-app.get('/profile/:userId', (req, res) => {
-	const { userId } = req.params;
-	db('users')
-	.where('id', userId)
-	.then(user => {
-		if(user.length) {
-			res.json(user[0]);
-		} else {
-			res.status(400).json('user not found');
-		}
-	})
-	.catch(err => res.status(400).json('error getting user'))
-}) 
-
-app.put('/image', (req, res) => {
-	const { id } = req.body;
-	db('users')
-		.where('id', '=', id)
-		.increment('entries', 1)
-		.returning('entries')
-		.then(entries => {
-			if(entries.length) {
-				res.json(Number(entries[0]));
-			} else {
-				res.status(400).json('user not found');
-			}
-		})
-		.catch(err => res.status(400).json('error getting user'))
-})
-
+app.post('/signin', signin.handleSignin(bcrypt, db));
+app.post('/signup', signup.handleSignup(bcrypt, db));
+app.get('/profile/:userId', profile.handleProfile(db)); 
+app.put('/image', image.handleImage(db));
 app.listen(3001, () => console.log('app is running on port 3001'));
